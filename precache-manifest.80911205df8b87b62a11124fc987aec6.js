ff264816fbcdd36569cf561863d05b674c5fe238
David Bienose <davidbienose@gmail.com>
2020-02-10 14:36:49 +0100

    Updates

---
+++ precache-manifest.80911205df8b87b62a11124fc987aec6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392b6e6bfab7e89e5b0e33e87bb89afc",
+    "url": "/monster-profile/index.html"
+  },
+  {
+    "revision": "71acbfdef11741923e84",
+    "url": "/monster-profile/static/css/main.4afadb50.chunk.css"
+  },
+  {
+    "revision": "228f5707f7a5e47747ef",
+    "url": "/monster-profile/static/js/2.d0e35b3a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-profile/static/js/2.d0e35b3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71acbfdef11741923e84",
+    "url": "/monster-profile/static/js/main.f348f523.chunk.js"
+  },
+  {
+    "revision": "9d0a58fd96d6c03490fd",
+    "url": "/monster-profile/static/js/runtime-main.791b2450.js"
+  }
+]);
